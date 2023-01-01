dd3346c630c979df0549ebfcabdc0e1899e71a57
smolugu <shyamprasad.molugu@gmail.com>
2022-12-31 19:41:53 -0500

    updates

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def main():
     init_db()  # initialize database if needed
     
     if MODE == "BACKTEST":
-        run_quick_backtest("2026-03-02")
+        run_quick_backtest("2026-03-03")
         return
     token = os.getenv("BOT_TOKEN")
     application = ApplicationBuilder().token(token).build()
